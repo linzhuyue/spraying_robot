1889abb8f514839ec74ea92b56b6894a1ed2b80e
Musyue <cboy@mail.ustc.edu.cn>
2019-07-11 11:14:31 +0800

    1,add ros and aubo test code

---
+++ aubo_driver/libpyauboi5-v1.2.2.x64/move_aubo_to_a_point_v1.py
@@ -0,0 +1,140 @@
+#! /usr/bin/env python
+# coding=utf-8
+import math
+from robotcontrol import *
+import rospy
+from std_msgs.msg import String,Float64,Bool
+class Test():
+    def __init__(self):
+        self.OpenstateBool=[False]
+        rospy.init_node("move_aubo_trajectory_planning_test")
+        self.OpenState = rospy.Subscriber("/open_aubo_state_flag", Bool, self.Open_Aubo_callback)
+    def Open_Aubo_callback(self,msg):
+        if len(self.OpenstateBool)>10:
+            self.OpenstateBool=self.OpenstateBool[1:]
+            self.OpenstateBool.append(msg.data)
+        else:
+            self.OpenstateBool.append(msg.data)
+        # print msg
+        self.Openstate=msg.data
+
+def main_1(test_count):
+    # 初始化logger
+    logger_init()
+
+    # 启动测试
+    logger.info("{0} test beginning...".format(Auboi5Robot.get_local_time()))
+
+    # 系统初始化
+    Auboi5Robot.initialize()
+
+    # 创建机械臂控制类
+    robot = Auboi5Robot()
+
+    # 创建上下文
+    handle = robot.create_context()
+
+    # 打印上下文
+    logger.info("robot.rshd={0}".format(handle))
+    try:
+
+        # 链接服务器
+        ip = '192.168.1.11'
+        port = 8899
+        result = robot.connect(ip, port)
+
+        if result != RobotErrorType.RobotError_SUCC:
+            logger.info("connect server{0}:{1} failed.".format(ip, port))
+        else:
+            # # 重新上电
+            # robot.robot_shutdown()
+            #
+            # # 上电
+            robot.robot_startup()
+            #
+            # # 设置碰撞等级
+            # robot.set_collision_class(7)
+
+            # 设置工具端电源为１２ｖ
+            # robot.set_tool_power_type(RobotToolPowerType.OUT_12V)
+
+            # 设置工具端ＩＯ_0为输出
+            robot.set_tool_io_type(RobotToolIoAddr.TOOL_DIGITAL_IO_0, RobotToolDigitalIoDir.IO_OUT)
+
+            # 获取工具端ＩＯ_0当前状态
+            tool_io_status = robot.get_tool_io_status(RobotToolIoName.tool_io_0)
+            logger.info("tool_io_0={0}".format(tool_io_status))
+
+            # 设置工具端ＩＯ_0状态
+            robot.set_tool_io_status(RobotToolIoName.tool_io_0, 1)
+
+            # 获取控制柜用户DI
+            io_config = robot.get_board_io_config(RobotIOType.User_DI)
+
+            # 输出DI配置
+            logger.info(io_config)
+
+            # 获取控制柜用户DO
+            io_config = robot.get_board_io_config(RobotIOType.User_DO)
+
+            # 输出DO配置
+            logger.info(io_config)
+
+            # 当前机械臂是否运行在联机模式
+            logger.info("robot online mode is {0}".format(robot.is_online_mode()))
+
+            # 循环测试
+            while test_count > 0:
+                test_count -= 1
+
+                joint_status = robot.get_joint_status()
+                logger.info("joint_status={0}".format(joint_status))
+
+                # 初始化全局配置文件
+                robot.init_profile()
+
+                # 设置关节最大加速度
+                # robot.set_joint_maxacc((5.5, 5.5, 5.5, 5.5, 5.5, 5.5))
+                #
+                # # 设置关节最大加速度
+                # robot.set_joint_maxvelc((1.5, .5, 2.5, 2.5, 2.5, 2.5))
+                # 设置关节最大加速度
+                robot.set_joint_maxacc((3.5, 3.5, 3.5, 3.5, 3.5, 3.5))
+
+                # 设置关节最大加速度
+                robot.set_joint_maxvelc((2.5, 2.5, 2.5, 2.5, 2.5, 2.5))
+                # 设置机械臂末端最大线加速度(m/s)
+                robot.set_end_max_line_acc(0.5)
+                logger.info("-------go-----to-----start-------step--01")
+                # 获取机械臂末端最大线加速度(m/s)
+                # robot.set_end_max_line_velc(0.2)
+                robot.set_end_max_line_velc(0.5)
+                joint_radian = deg_to_rad((10.3,3.6,-89.9,-91.2,-86.04,0.15))#((0,0,0,0,0,0))
+                logger.info("move joint to {0}".format(joint_radian))
+                robot.move_joint(joint_radian)
+
+
+            # 断开服务器链接
+            robot.disconnect()
+
+    except RobotError, e:
+        logger.error("{0} robot Event:{1}".format(robot.get_local_time(), e))
+
+    finally:
+        # 断开服务器链接
+        if robot.connected:
+            # 关闭机械臂
+            robot.robot_shutdown()
+            # 断开机械臂链接
+            robot.disconnect()
+        # 释放库资源
+        Auboi5Robot.uninitialize()
+        logger.info("{0} test completed.".format(Auboi5Robot.get_local_time()))
+def main():
+    ak=Test()
+    if ak.OpenstateBool[-1]:
+        main_1(1)
+
+if __name__=="__main__":
+    main()
+    # print deg_to_rad((-3.3364,12.406,-81.09,-91.207,-86.08,0.164))
